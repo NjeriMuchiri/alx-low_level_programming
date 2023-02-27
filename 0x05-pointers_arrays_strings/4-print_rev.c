e1c64d001ef2ba9fa723cfed3de20b45e9a42c9c
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-02-27 05:15:13 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_rev - prints string in reverse
+ *@s: input string
+ *Return: no return
+ */
+void print_rev(char *s)
+{
+int count = 0;
+while (count >= 0)
+{
+if (s[count] == '0')
+break;
+count++;
+}
+for (count--; count >= 0; count--)
+putchar(s[count]);
+putchar('\n');
+}
