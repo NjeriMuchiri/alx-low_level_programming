ae4a8885f0cc724a6cfb6183a4e454930efce0e8
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-03-02 01:40:07 -0800

    comparison strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,8 +3,8 @@
  *_strcmp - compares two strings
  *@s1: first string
  *@s2: second string
- *Return 0 if s1 and s2 are equals,
- *another number ifotherwise
+ *Return: 0 if s1 and s2 are equals,
+ *another number if otherwise
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -14,7 +14,7 @@ while (op == 0)
 if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 break;
 op = *(s1 + i) - *(s2 + i);
- i++;
+i++;
 }
 return (op);
 }
