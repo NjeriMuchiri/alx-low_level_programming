2b46ccf96cb03bd9e6f077b59255a0e4ca4ccdd0
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-03-10 02:17:49 -0800

    args multiplication

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - prints all the arguments it receives
- *@argc: numberof  the command line arguments
- *@argv: array containing the program command line arguments passed 
- *Return: 0 - success
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
