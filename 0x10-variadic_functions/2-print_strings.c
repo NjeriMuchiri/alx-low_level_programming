db4e5c1d5a7405f154ebb2e45ca1f78e1b6f51c8
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-03-23 06:41:47 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
  *print_strings - function that prints strings followed by a new line
  *@separator: string between strings
  *@n - number of strings passed to the function
- *Return: nothing
+ *Return: no return
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
