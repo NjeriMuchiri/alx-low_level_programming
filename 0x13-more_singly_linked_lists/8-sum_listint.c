8201b6c0a33a4b4cf802494f9281a296cfbe603f
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-04-04 07:24:29 -0700

    adding the data in the linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *sum_listint - function that returns the sum of all data of a linked list
+ *@head: head of the list
+ *Return:sum of all the data (n)
+ *if the list is empty return 0
+ */
+int sum_listint(listint_t *head)
+{
+int sum;
+sum = 0;
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);  
+}
