e3675826ace67f45f15e6127a8ee61ffe0994473
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-04-06 07:49:28 -0700

    set bits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,6 +4,7 @@
  *at a given index
  *@n:pointer of  unsigned long int
  *@index: bit index starting from 0
+ *Return: 1 if it worked else -1 if errornous
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
