4e7ac0524ed9996aabef4d90d9d4609a8cc62379
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-18 21:50:51 +0300

    deletion of node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,9 +2,9 @@
 /**
  * delete_dnodeint_at_index - deletes the node at index
  * of a dlistint_t linked list
- * @head - head of the linked lists
- * @index - index of the new node
- * Return - 1 if it succeeded, -1 if it fails
+ * @head: head of the linked lists
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it fails
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
