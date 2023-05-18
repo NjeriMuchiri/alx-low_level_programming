8be287c6405b2d51024266727cff51992f45ce10
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-18 21:54:21 +0300

    sum of nodes

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum__dlistint - returns the sum of all the data
+ * sum_dlistint - returns the sum of all the data
  * of a dlistint_t
  * @head: head of the list
  * Return: sum of the data else return 0 if list is empty
