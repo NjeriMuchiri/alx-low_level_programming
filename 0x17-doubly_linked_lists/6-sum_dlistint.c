f0e58f2d4c7a8aeb4431b3022f62180431ebba64
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-18 21:38:43 +0300

    sum of the nodes

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,21 +3,15 @@
  * sum__dlistint - returns the sum of all the data
  * of a dlistint_t
  * @head: head of the linked list
- * Return: sum of the data
+ * Return: sum of the data else return 0 if list is empty
 */
 int sum_dlistint(dlistint_t *head)
 {
-int sum;
-sum = 0;
-if (head != NULL)
-{
-while (head->prev != NULL)
-head = head->prev;
-while (head != NULL)
+int sum = 0;
+while (head)
 {
 sum += head->n;
 head = head->next;
-}
 }
 return (sum);
 }
