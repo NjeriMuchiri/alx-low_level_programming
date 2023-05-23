dd824a5eeabdb74231192fdf6880ab0377d430a3
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-23 17:32:06 +0300

    getting the opcodes and creating the other functions

--- monty/monty.h
+++ monty/monty.h
@@ -63,4 +63,35 @@ typedef struct globals
 
 extern global_varia vglo;
 
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **doubly, unsigned int cline);
+void _pop(stack_t **doubly, unsigned int cline);
+void _swap(stack_t **doubly, unsigned int cline);
+void _queue(stack_t **doubly, unsigned int cline);
+void _stack(stack_t **doubly, unsigned int cline);
+void _add(stack_t **doubly, unsigned int cline);
+void _nop(stack_t **doubly, unsigned int cline);
+void _sub(stack_t **doubly, unsigned int cline);
+void _div(stack_t **doubly, unsigned int cline);
+void _mul(stack_t **doubly, unsigned int cline);
+void _mod(stack_t **doubly, unsigned int cline);
+void _pchar(stack_t **doubly, unsigned int cline);
+void _pstr(stack_t **doubly, unsigned int cline);
+void _rotl(stack_t **doubly, unsigned int cline);
+void _rotr(stack_t **doubly, unsigned int cline);
+
+/*get function*/
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+/*function_strings*/
+int _sch(char *s, char c);
+char *_strtoky(char *s, char *d);
+int _strcmp(char *s1, char *s2);
+/*functions_size*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* main */
+void free_vglo(void);
+
 #endif /*MONTY_H*/
