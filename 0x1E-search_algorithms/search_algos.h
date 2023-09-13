f51c268b4a188c10d95610979caff90c18225a33
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-13 11:17:40 +0300

    interpolation search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -41,9 +41,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-t_listint *jump_list(t_listint *list, size_t size, int value);
-listing_t *linear_skip(listing_t *list, int value);
 
 #endif
